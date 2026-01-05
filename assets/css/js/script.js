33675ba291c52b907999c36e7e2768df4fa3fd37
YandiJulian <141604509+YandiJulian@users.noreply.github.com>
2026-01-06 01:29:28 +0700

    Add files via upload

---
+++ assets/css/js/script.js
@@ -0,0 +1,7 @@
+function toggleMenu() {
+document.getElementById('navMenu').classList.toggle('active');
+}
+
+function toggleMenu(){
+  document.querySelector('.nav-menu').classList.toggle('show');
+}
