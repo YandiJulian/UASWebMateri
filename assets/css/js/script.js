eb4b37756aa70b8778cb3c35989c3cd34b710fc3
YandiJulian <141604509+YandiJulian@users.noreply.github.com>
2026-01-06 01:51:28 +0700

    Refactor toggleMenu function for consistency

--- assets/css/js/script.js
+++ assets/css/js/script.js
@@ -4,4 +4,9 @@ document.getElementById('navMenu').classList.toggle('active');
 
 function toggleMenu(){
   document.querySelector('.nav-menu').classList.toggle('show');
+
+}
+
+function toggleMenu(){
+  document.getElementById("navMenu").classList.toggle("show");
 }
